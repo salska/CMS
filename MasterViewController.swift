93378fe62838fb79d130d6d3c0f5c72819096d73
salska <sal@emetrix.biz>
2016-12-20 20:45:10 +0000

    Initial Commit

---
+++ MasterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.swift
+//  ScaryBugsMac
+//
+//  Created by Sal Kabay on 16/03/2016.
+//  Copyright © 2016 Sal Kabay. All rights reserved.
+//
+
+import Cocoa
+
+class MasterViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
